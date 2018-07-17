a945a9af65a5e516bb70db68625026b34d18cba9
Luova Labs <hello@luovalabs.com>
2018-07-17 13:01:33 -0400

    Setup FCIB Connection to API

--- src/services/transaction.services.ts
+++ src/services/transaction.services.ts
@@ -1,94 +1,34 @@
-import { Component, Injectable } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import * as Config from '../config';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/forkJoin';
 
 @Injectable()
 export class TransactionServices {
 
-    pages: Array<{title: string, component: any, icon: string}>;
-    transactionInfo: Array<{id: number, text: string}>;
-    
-    private transaction_information = [{
-        recipient_first_name: '',
-        recipient_last_name: '',
-        recipient_address:'',
-        recipient_nationality:'',
-        recipient_tel:'',
-        recipient_streetAddress:'',
-        recipient_country:'',
-        recipient_postal_code:'',
-        recipient_city:'',
-        recipient_transaction_amount:'',
-        }
-    ]
+  public user:any;
 
-    private recipient_errors = [{
-        firstname_error: 'First Name is required',
-        lastname_error: 'Last Name is required',
-        tel_error: 'Telelphone Number is required',
-        street_address_error: 'Street Address is required',
-        }
-    ]
+  constructor(public http: Http){}
 
-    constructor(public storage:Storage, public navCtrl: NavController) {
+  fetchAccessToken(){
+    let header: Headers = new Headers();
+    header.append('Authorization', 'Basic aWItZXh0OjVjYzEwZGNjLTc4NjctNGRhMy1hNDMyLTVhMzc0NTQ3YjY3Mg==');
+    header.append('Content-Type', 'application/json');
 
-    }
+    return this.http.post(Config.HOST + "/ob/auth/v1/auth/token",{ headers: header })
+    .map(res => res.json());
+  }
 
-    public setFirstName(firstname){
-        this.transaction_information[0].recipient_first_name = firstname;
+  userLogin(){
+    let header: Headers = new Headers();
+    header.append('Authorization', 'Bearer ' + this.user.access_token);
+    header.append('Content-Type', 'application/json');
 
-    }
+    return this.http.get(Config.HOST + "/ob/account-balance/v1/accounts/balances",{ headers: header })
+    .map(res => res.json());
 
-    public setLastName(lastname){
-        this.transaction_information[0].recipient_last_name = lastname;
-    }
-
-    /*public setTel(tel){
-        this.transaction_information[0].recipient_tel = tel;
-    }
-    
-    public setNationality(nationality){
-        this.transaction_information[0].recipient_nationality = nationality;
-    }
-
-    public setStreetAddress(streetAddress){
-        this.transaction_information[0].recipient_streetAddress = streetAddress;
-    }
-
-    public setCountry(country){
-        this.transaction_information[0].recipient_country = country;
-    }
-
-    public setPostalCode(postal_code){
-        this.transaction_information[0].recipient_postal_code = postal_code;
-    }
-
-    public setCity(city){
-        this.transaction_information[0].recipient_city = city;
-    }
-
-    public setTransactionAmount(transaction){
-        this.transaction_information[0].recipient_transaction_amount = transaction;
-    }
-
-    public getRecipientErrors(){
-        return this.recipient_errors;
-    }*/
-    
-    public saveDetails(){
-        console.log(this.transaction_information);
-        this.storage.set("transaction_information",this.transaction_information);
-    }
-
-    public getDetails(){
-        
-        /*this.storage.get('transaction_information').then((val) => {
-            console.log('Your val is', val);
-        });*/
-
-    }
+  }
 
 }
-
-
-  
